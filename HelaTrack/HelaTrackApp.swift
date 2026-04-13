6e556c4081332d49d75b8029a1417e45150c2a3a
mangochops <whymangoeswillicent@gmail.com>
2026-04-13 11:40:29 +0300

    Initial Commit

---
+++ HelaTrack/HelaTrackApp.swift
@@ -0,0 +1,17 @@
+//
+//  HelaTrackApp.swift
+//  HelaTrack
+//
+//  Created by mac on 4/13/26.
+//
+
+import SwiftUI
+
+@main
+struct HelaTrackApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
